4bf683fa9c00844be506f562fd4f255f9ee6b36f
petrificustotalus <natalka_nowak@tlen.pl>
2021-02-12 15:53:33 +0100

    search-results route added

--- views.py
+++ views.py
@@ -1,6 +1,15 @@
 from flask import render_template
 from app import app
 
+
+# startpage route - localhost:5000/blog/
 @app.route("/")
 def home():
     return render_template('home.jinja2')
+
+
+# searchresults route - localhost:5000/<slug>/
+@app.route("/search_results")
+def search_results():  # will take 'drugname'
+    # drug = Drug.query.filter(Drug.name==drugname).first()
+    return render_template('search_results.html')  # drug=drug
