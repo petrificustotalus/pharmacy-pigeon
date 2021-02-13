a6b41bb55877c92b5246f1bcd599a119bf1a0b4f
petrificustotalus <natalka_nowak@tlen.pl>
2021-02-13 13:53:57 +0100

    2 Pharmacy records added

--- rundb.py
+++ rundb.py
@@ -10,15 +10,15 @@ from models import *
 
 # d = Drug(name='Ketonal', size=50, power=100, state='tabs', prescription=True, refundation=0)
 
-d1 = Drug(name='Allertec', size=100, power=1, state='syrop', prescription=False, refundation=30)
-d2 = Drug(name='Zyrtec', size=20, power=10, state='tabs', prescription=True, refundation=0)
-d3 = Drug(name='Groprinosin', size=20, power=500, state='tabs', prescription=False, refundation=0)
-d4 = Drug(name='Apap', size=12, power=500, state='tabs', prescription=False, refundation=0)
+# d1 = Drug(name='Allertec', size=100, power=1, state='syrop', prescription=False, refundation=30)
+# d2 = Drug(name='Zyrtec', size=20, power=10, state='tabs', prescription=True, refundation=0)
+# d3 = Drug(name='Groprinosin', size=20, power=500, state='tabs', prescription=False, refundation=0)
+# d4 = Drug(name='Apap', size=12, power=500, state='tabs', prescription=False, refundation=0)
 
+p1 = Pharmacy(name='New Pharmacy', phone=777666543, adress="ul. Młynowa 61")
+p2 = Pharmacy(name='Extra Cool Pharmacy', phone=777666531, adress="ul. Młynowa 62")
 
-db.session.add(d1)
-db.session.add(d2)
-db.session.add(d3)
-db.session.add(d4)
+db.session.add(p1)
+db.session.add(p2)
 db.session.commit()
 
