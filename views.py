068029f35065816920ca41d61ba1e4f76620b9ef
petrificustotalus <natalka_nowak@tlen.pl>
2021-02-17 03:08:05 +0100

    sending ClientDataForm creates new record in Client table

--- views.py
+++ views.py
@@ -1,25 +1,42 @@
 from flask import render_template, request, redirect, url_for
 from app import app, db
-from models import Druginfo, DrugItem
+from models import Druginfo, DrugItem, Client
 from forms import SearchForm, ClientDataForm
 
 
 # startpage route - localhost:5000/blog/
-@app.route("/", methods=['POST', 'GET'])
+@app.route("/", methods=["POST", "GET"])
 def home():
     form = SearchForm()
 
-    if request.method == 'POST':
+    if request.method == "POST":
         # city = request.form.get('city')
-        drugname = request.form.get('drugname')
-        return redirect(url_for('search_results', drugname=drugname))
+        drugname = request.form.get("drugname")
+        return redirect(url_for("search_results", drugname=drugname))
 
-    return render_template('home.jinja2', form=form)
+    return render_template("home.jinja2", form=form)
 
 
 # searchresults route - localhost:5000/<slug>/
-@app.route("/search_results/<drugname>", methods=['POST', 'GET'])
+@app.route("/search_results/<drugname>", methods=["POST", "GET"])
 def search_results(drugname):  # will take 'drugname'
-    drug = Druginfo.query.filter(Druginfo.name==drugname).first()
+    drug = Druginfo.query.filter(Druginfo.name == drugname).first()
     form = ClientDataForm()
-    return render_template('search_results.html', drug=drug, form=form)
+    if request.method == "POST":
+        name = request.form.get("name")
+        surname = request.form.get("surname")
+        email = request.form.get("email")
+        phone = request.form.get("phone")
+        address = request.form.get("address")
+
+        try:
+            client = Client(
+                name=name, surname=surname, email=email, phone=phone, address=address
+            )
+            db.session.add(client)
+            db.session.commit()
+        except:
+            print("Very long traceback")
+        return redirect(url_for("home"))
+
+    return render_template("search_results.html", drug=drug, form=form)
