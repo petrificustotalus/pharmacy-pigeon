de9dc31107e8ededb9d7753537f60dbbf521bc9f
petrificustotalus <natalka_nowak@tlen.pl>
2021-02-17 03:55:19 +0100

    reservation form in progress

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import Form, StringField, SubmitField
+from wtforms import Form, StringField, SubmitField, IntegerField, BooleanField
 from wtforms.validators import DataRequired, Email
 
 class SearchForm(Form):
@@ -11,6 +11,13 @@ class ClientDataForm(FlaskForm):
     name = StringField('Imię', validators=[DataRequired()])
     surname = StringField('Nazwisko', validators=[DataRequired()])
     email = StringField('E-mail', validators=[DataRequired(), Email()])
-    phone = StringField('Telefon kontaktowy')
+    phone = IntegerField('Telefon kontaktowy')
     address = StringField('Adres')
     submit = SubmitField('Potwierdź dane')
+
+
+class ReservationForm(FlaskForm):
+    # należy zmienić quantity na okno (domyślnie 1 opakowanie) z - i + do manipulowania ilością
+    quantity = IntegerField('Wprowadź liczbę opakowań', validators=[DataRequired()])
+    prescription = BooleanField('Oświadczam że posiadam ważną receptę do zamówionych leków na receptę.')
+    submit = SubmitField('Złóż zamówienie')
--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 from flask import render_template, request, redirect, url_for
 from app import app, db
 from models import Druginfo, DrugItem, Client
-from forms import SearchForm, ClientDataForm
+from forms import SearchForm, ClientDataForm, ReservationForm
 
 
 # startpage route - localhost:5000/blog/
@@ -22,21 +22,22 @@ def home():
 def search_results(drugname):  # will take 'drugname'
     drug = Druginfo.query.filter(Druginfo.name == drugname).first()
     form = ClientDataForm()
-    if request.method == "POST":
-        name = request.form.get("name")
-        surname = request.form.get("surname")
-        email = request.form.get("email")
-        phone = request.form.get("phone")
-        address = request.form.get("address")
+    reservation_form = ReservationForm()
+    # if request.method == "POST":
+    #     name = request.form.get("name")
+    #     surname = request.form.get("surname")
+    #     email = request.form.get("email")
+    #     phone = request.form.get("phone")
+    #     address = request.form.get("address")
 
-        try:
-            client = Client(
-                name=name, surname=surname, email=email, phone=phone, address=address
-            )
-            db.session.add(client)
-            db.session.commit()
-        except:
-            print("Very long traceback")
-        return redirect(url_for("home"))
+    #     try:
+    #         client = Client(
+    #             name=name, surname=surname, email=email, phone=phone, address=address
+    #         )
+    #         db.session.add(client)
+    #         db.session.commit()
+    #     except:
+    #         print("Very long traceback")
+    #     return redirect(url_for("home"))
 
-    return render_template("search_results.html", drug=drug, form=form)
+    return render_template("search_results.html", drug=drug, form=form, reservation_form=reservation_form)
