766e42b144152d8e94bffeaa2f0c48de3288043c
petrificustotalus <natalka_nowak@tlen.pl>
2021-02-23 00:49:06 +0100

    add_ordet route added, form working, records creates in orders table

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect, url_for
 from app import app, db
-from models import Druginfo, DrugItem, Client, Pharmacy
+from models import Druginfo, DrugItem, Client, Pharmacy, Order
 from forms import SearchForm, ClientDataForm
 
 
@@ -16,36 +16,34 @@ def home():
 
     return render_template("home.jinja2", form=form)
 
-
-# def add_client(client):
-    # tu trzeba napisać funkcje która 
-    # 1 sprawdza czy dane nie pokrywają się z jakimś rekordem w bazie 
-    # 2 jeśli tak zwraca id tego klienta, jeśli nie - dodaje rekord do ClientTable i zwraca id tego nowego klienta
-
-# def add_order(client, order)
-    # ta funkcja ma tworzyć nowy rekord w tabeli orders przypisując go do klienta ^
-
-
 # searchresults route - localhost:5000/<slug>/
 @app.route("/search_results/<drugname>", methods=["POST", "GET"])
 def search_results(drugname):  # will take 'drugname'
     drug = Druginfo.query.filter(Druginfo.name == drugname).first()
     form = ClientDataForm()
-    if request.method == "POST":
-        name = request.form.get("name")
-        surname = request.form.get("surname")
-        email = request.form.get("email")
-        phone = request.form.get("phone")
-        address = request.form.get("address")
-
-        try:
-            client = Client(
-                name=name, surname=surname, email=email, phone=phone, address=address
-            )
-            db.session.add(client)
-            db.session.commit()
-        except:
-            print("Very long traceback")
-        return redirect(url_for("home"))
 
     return render_template("search_results.html", drug=drug, form=form)
+
+
+@app.route("/order/<drugitem_id>", methods=["POST"])
+def add_order(drugitem_id):
+    # this part adds new client to Client table (it supposed to verify if the client doesn't already exist)
+    name = request.form.get("name")
+    surname = request.form.get("surname")
+    email = request.form.get("email")
+    phone = request.form.get("phone")
+    address = request.form.get("address")
+
+    try:
+        client = Client(
+            name=name, surname=surname, email=email, phone=phone, address=address
+        )
+        db.session.add(client)
+        db.session.commit()
+    except:
+        print("Very long traceback")
+    # this part adds order to the order table
+    order = Order(client_id=client.id, drugitem_id=drugitem_id)
+    db.session.add(order)
+    db.session.commit()
+    return redirect(url_for("home"))
