944af1a51dfc7fa88405bdc12ede592528110563
petrificustotalus <natalka_nowak@tlen.pl>
2021-03-25 04:14:19 +0100

    db_clear() deletes expired orders and also sands annulation email

--- utils.py
+++ utils.py
@@ -1,7 +1,13 @@
-from app import db
+from app import db, mail
 from models import Druginfo, DrugItem, Client, Pharmacy, Order
 from datetime import datetime, timedelta
 from time import time
+from flask_mail import Message
+
+def send_annulation(email):
+    msg = Message('Anulowano rezerwację', sender='natalka_nowak@tlen.pl ', recipients=[email])
+    msg.body = f''' Twoja rezerwacja leków została anulowana '''
+    mail.send(msg)
 
 def db_clear():
     two_days = timedelta(days=2)
@@ -10,6 +16,9 @@ def db_clear():
     orders_expired = Order.query.filter(Order.date_ordered <= expiration_time).all()
     print(orders_expired)
     for order in orders_expired:
+        client = Client.query.filter(Client.id == order.client_id).first()
+        email = client.email
+        send_annulation(email)
         db.session.delete(order)
         db.session.commit()
     print(orders_expired)
