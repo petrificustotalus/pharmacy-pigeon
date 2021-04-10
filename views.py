74344f963e6c4f5f01200b55935d818f914a3196
petrificustotalus <natalka_nowak@tlen.pl>
2021-04-10 15:33:30 +0200

    functions: reduce_from_cart, increase_from_cart and remove_cart_item, flask_session added to gitignore

--- views.py
+++ views.py
@@ -12,6 +12,24 @@ drugs = ["Allertec", "Ketonal", "Ketoprofen"]
 def autocomplete():
     return Response(json.dumps(drugs), mimetype='application/json')
 
+def reduce_from_cart(id):
+    for cart_item in session["cart"]:
+        if cart_item['drug_id'] == id:
+            if cart_item['quantity'] > 1:
+                cart_item['quantity'] -= 1
+            else:
+                session["cart"].remove(cart_item)
+
+def increase_from_cart(id):
+    for cart_item in session["cart"]:
+        if cart_item['drug_id'] == id:
+            cart_item['quantity'] += 1
+
+def delete_cart_item(id):
+    for cart_item in session["cart"]:
+        if cart_item['drug_id'] == id:
+            session["cart"].remove(cart_item)
+
 @app.route("/cart", methods=["GET", "POST"])
 def cart():
 
